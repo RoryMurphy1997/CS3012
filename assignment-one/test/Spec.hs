08aaacfe2ced0aece6b0047c55372cc0b24ae834
RoryMurphy1997 <43140750+RoryMurphy1997@users.noreply.github.com>
2018-09-19 16:34:56 +0100

    Created some basic unit tests in the test file that I believe will be relevant.

--- assignment-one/test/Spec.hs
+++ assignment-one/test/Spec.hs
@@ -1,2 +1,26 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+
+import           Lib
+import           Test.Hspec
+import           Test.QuickCheck
+
+
+
+
+spec :: Spec
+spec = do
+  describe "Lowest Common Ancestor" $ do
+
+    it "returns nothing for no input" $ do
+      (lowestCommonAncestor ) 'shouldBe' Nothing
+
+    it "returns nothing for only a tree input" $ do
+      (lowestCommonAncestor tree ) 'shouldBe' Nothing
+
+    it "returns itself for only one input" $ do
+      (lowestCommonAncestor tree 5) == 5 'shouldBe' True
+
+    it "returns 1 for inputs 2 and 3" $ do
+      (lowestCommonAncestor tree 2 3) == 1 'shouldBe' True
+
+    it "returns 1 if one of the inputs is 1 and the other is a child of 1" $ do
+      (lowestCommonAncestor tree 1 2) == 1 'shouldBe' True
