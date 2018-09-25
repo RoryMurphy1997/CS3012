cee772e699c38ee8cba516e70df4c38a11a4616f
RoryMurphy1997 <43140750+RoryMurphy1997@users.noreply.github.com>
2018-09-25 14:18:18 +0100

    Added dependencies to package.yaml and updated Spec.hs commas

--- assignment-one/test/Spec.hs
+++ assignment-one/test/Spec.hs
@@ -1,3 +1,4 @@
+module Spec where
 
 import Lib
 import Test.Hspec
@@ -9,13 +10,13 @@ spec = do
   describe "Lowest Common Ancestor" $ do
 
     it "returns nothing for no input" $ do
-      (lowestCommonAncestor ) 'shouldBe' Nothing
+      (lowestCommonAncestor ) `shouldBe` Nothing
 
     it "returns nothing for only a tree input" $ do
-      (lowestCommonAncestor tree ) 'shouldBe' Nothing
+      (lowestCommonAncestor tree ) `shouldBe` Nothing
 
     it "returns itself for only one input" $ do
-      (lowestCommonAncestor tree 5) == 5 'shouldBe' True
+      (lowestCommonAncestor tree 5) == 5 `shouldBe' True
 
     it "returns 1 for inputs 2 and 3" $ do
       (lowestCommonAncestor tree 2 3) == 1 'shouldBe' True
