0d35d052856f7b049980836e8651bcfcbe5509d1
RoryMurphy1997 <43140750+RoryMurphy1997@users.noreply.github.com>
2018-09-25 14:33:58 +0100

    Updated test cases to match code in Lib

--- assignment-one/test/Spec.hs
+++ assignment-one/test/Spec.hs
@@ -4,22 +4,32 @@ import Lib
 import Test.Hspec
 import Test.QuickCheck
 
+data Tree a = Empty | Node a (Tree a) (Tree a) deriving Show
+
+myTree :: Tree Int
+myTree = Node 1
+    (Node 2
+        (Node 4 Empty Empty)
+        (Node 5 Empty Empty))
+    (Node 3
+        (Node 6 Empty Empty)
+        (Node 7 Empty Empty))
 
 spec :: Spec
 spec = do
   describe "Lowest Common Ancestor" $ do
 
     it "returns nothing for no input" $ do
-      (lowestCommonAncestor ) `shouldBe` Nothing
+      (lca ) `shouldBe` Nothing
 
     it "returns nothing for only a tree input" $ do
-      (lowestCommonAncestor tree ) `shouldBe` Nothing
+      (lca myTree ) `shouldBe` Nothing
 
     it "returns itself for only one input" $ do
-      (lowestCommonAncestor tree 5) == 5 `shouldBe' True
+      (lca myTree 5) == 5 `shouldBe` True
 
     it "returns 1 for inputs 2 and 3" $ do
-      (lowestCommonAncestor tree 2 3) == 1 'shouldBe' True
+      (lca myTree 2 3) == 1 `shouldBe` True
 
     it "returns 1 if one of the inputs is 1 and the other is a child of 1" $ do
-      (lowestCommonAncestor tree 1 2) == 1 'shouldBe' True
+      (lca myTree 1 2) == 1 `shouldBe` True
