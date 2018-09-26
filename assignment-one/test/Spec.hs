1ad1092f5443bf61f16373c4f6f37e27a7feb474
RoryMurphy1997 <43140750+RoryMurphy1997@users.noreply.github.com>
2018-09-26 16:49:38 +0100

    Added additional tests and updated wording

--- assignment-one/test/Spec.hs
+++ assignment-one/test/Spec.hs
@@ -6,30 +6,36 @@ import Test.QuickCheck
 
 data Tree a = Empty | Node a (Tree a) (Tree a) deriving Show
 
-myTree :: Tree Int
-myTree = Node 1
-    (Node 2
-        (Node 4 Empty Empty)
-        (Node 5 Empty Empty))
-    (Node 3
-        (Node 6 Empty Empty)
-        (Node 7 Empty Empty))
 
-spec :: Spec
-spec = do
+
+spec :: IO ()
+spec = hspec $ do
   describe "Lowest Common Ancestor" $ do
 
-    it "returns nothing for no input" $ do
-      (lca ) `shouldBe` Nothing
+    it "returns nothing for no input" $
+      lca_show  `shouldBe` Nothing
 
-    it "returns nothing for only a tree input" $ do
-      (lca myTree ) `shouldBe` Nothing
+    it "returns nothing for only a tree input" $
+      lca_show myTree  `shouldBe` Nothing
 
-    it "returns itself for only one input" $ do
-      (lca myTree 5) == 5 `shouldBe` True
+    it "returns itself for only one input" $
+      lca_show myTree 5 == 5 `shouldBe` True
 
-    it "returns 1 for inputs 2 and 3" $ do
-      (lca myTree 2 3) == 1 `shouldBe` True
+    it "returns parent for input of its two children" $
+      lca_show myTree 2 3 == 1 `shouldBe` True
 
-    it "returns 1 if one of the inputs is 1 and the other is a child of 1" $ do
-      (lca myTree 1 2) == 1 `shouldBe` True
+    it "returns parent if one of the inputs is parent and the other is a child" $
+      lca_show myTree 3 4 == 3 `shouldBe` True
+
+    it "returns grandparent for input of two of its grandchildren" $
+      lca_show myTree 4 7 == 1 `shouldBe` True
+
+
+    myTree :: Tree Int
+    myTree = Node 1
+        (Node 2
+            (Node 4 Empty Empty)
+            (Node 5 Empty Empty))
+        (Node 3
+            (Node 6 Empty Empty)
+            (Node 7 Empty Empty))
