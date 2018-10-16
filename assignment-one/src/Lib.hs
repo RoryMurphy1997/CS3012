7315c0cbaa70039d4ba32727045bac6bb624ba2d
RoryMurphy1997 <43140750+RoryMurphy1997@users.noreply.github.com>
2018-10-16 11:46:42 +0100

    Changes implementation so that it now runs

--- assignment-one/src/Lib.hs
+++ assignment-one/src/Lib.hs
@@ -1,14 +1,9 @@
 module Lib
-    ( cons,
-      lca,
-      ID,
+    ( lca,
       Path,
       someFunc
     ) where
 
-someFunc :: IO ()
-someFunc = do
-  putStrLn (lca a b)
 
 type Id = Int
 data Path = [Id] :# !Int
@@ -16,12 +11,15 @@ data Path = [Id] :# !Int
 empty :: Path
 empty = [] :# 0
 
---Paths a,b,c,d make up BST from previous solution
 a :: Path
 a = [4,2,1] :# 3
 
 b :: Path
 b = [5,2,1] :# 3
+
+someFunc :: IO ()
+someFunc = do
+  print x where ((x:xs) :# n) = (lca a b)
 
 cons :: Id -> Path -> Path
 cons a (ys :# n) = (a:ys) :# (n + 1)
@@ -29,6 +27,6 @@ cons a (ys :# n) = (a:ys) :# (n + 1)
 lca :: Path -> Path -> Path
 lca (xs0 :# i) (ys0 :# j) = go k (drop (i-k) xs0) (drop (j-k) ys0) where
   k = min i j
-  go !n xxs@(x:xs) (y:ys)
-    | x == y   = xxs :# n
-    | otherwse = go (n - 1) xs ys
+  go n xxs@(x:xs) (y:ys) = case(x) of
+                    (y) -> xxs :# n
+                    _   -> go (n-1) xs ys
