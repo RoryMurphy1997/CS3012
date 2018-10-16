26810d39d13573d4276534067b0cc04f6d1f8971
RoryMurphy1997 <43140750+RoryMurphy1997@users.noreply.github.com>
2018-10-16 11:51:49 +0100

    Updated tests to compare results to defined paths.

--- assignment-one/test/LibSpec.hs
+++ assignment-one/test/LibSpec.hs
@@ -9,25 +9,25 @@ spec = do
   describe "Lowest Common Ancestor" $ do
 
     it "returns empty if passed an Empty tree" $
-      lca empty empty == [] :# 0 `shouldBe` True
+      lca empty empty == empty `shouldBe` True
 
     it "returns empty if passed two nodes not in same graph" $
-      lca a f == [] :# 0 `shouldBe` True
+      lca a f == empty `shouldBe` True
 
     it "returns the node if the same node is input twice" $
-      lca b b == [5,2,1] :# 3 `shouldBe` True
+      lca b b == b `shouldBe` True
 
     it "returns lowest common ancestor for two different paths" $
-      lca c d == [2,1] :# 2 `shouldBe` True
+      lca c d == g `shouldBe` True
 
     it "returns full item 2 if path 1 is path 2's direct ancestor" $
-      lca a g == [2,1] :# 2 `shouldBe` True
+      lca a g == g `shouldBe` True
 
     it "returns path 2 levels up if two given paths differe by 2 items" $
-      lca a c == [1] :# 1 `shouldBe` True
+      lca a c == h `shouldBe` True
 
     it "returns single lowest common ancestor when more than one ancestor exists" $
-      lca c e == Right 1 `shouldBe` True
+      lca c e == h `shouldBe` True
 
 empty :: Path
 empty = [] :# 0
@@ -52,3 +52,6 @@ f = [11,12,13,14] :# 4
 
 g :: Path
 g = [2,1] :# 2
+
+h :: Path
+h = [1] :# 1
