6f4816d4ccf4acb60e1e23bf21b1a56a37cdfc5a
RoryMurphy1997 <43140750+RoryMurphy1997@users.noreply.github.com>
2018-10-18 16:08:10 +0100

    See previous

--- assignment-one/test/LibSpec.hs
+++ assignment-one/test/LibSpec.hs
@@ -9,28 +9,28 @@ spec = do
   describe "Lowest Common Ancestor" $ do
 
     it "returns empty if passed an Empty tree" $
-      lcaToString(lca empty empty) == [] `shouldBe` True
+      pathToArray(lca empty empty) == [] `shouldBe` True
 
     it "returns empty if passed two nodes not in same graph" $
-      lcaToString(lca a f) == [] `shouldBe` True
+      pathToArray(lca a f) == [] `shouldBe` True
 
     it "returns nothing even if connected at end" $
-      lcaToString(lca d e) == [] `shouldBe` True
+      pathToArray(lca d e) == [] `shouldBe` True
 
     it "returns the node if the same node is input twice" $
-      lcaToString(lca b b) == lcaToString(b) `shouldBe` True
+      pathToArray(lca b b) == pathToArray(b) `shouldBe` True
 
     it "returns lowest common ancestor for two different paths" $
-      lcaToString(lca c d) == lcaToString(g) `shouldBe` True
+      pathToArray(lca c d) == pathToArray(g) `shouldBe` True
 
     it "returns full item 2 if path 1 is path 2's direct ancestor" $
-      lcaToString(lca a g) == lcaToString(g) `shouldBe` True
+      pathToArray(lca a g) == pathToArray(g) `shouldBe` True
 
     it "returns path 2 levels up if two given paths differe by 2 items" $
-      lcaToString(lca a c) == lcaToString(h) `shouldBe` True
+      pathToArray(lca a c) == pathToArray(h) `shouldBe` True
 
     it "returns lowest common ancestor even when paths are joined further down" $
-      lcaToString(lca f h) == lcaToString(i) `shouldBe` True
+      pathToArray(lca f h) == pathToArray(i) `shouldBe` True
 
 
 
